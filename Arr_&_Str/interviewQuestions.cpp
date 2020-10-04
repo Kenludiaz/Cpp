e6f74adac59ae981df97bb90f217802994f56a05
Kenneth Lu Diaz <ludiazken@gmail.com>
2020-10-04 16:48:24 -0400

    Used set to answer first question.

---
+++ Arr_&_Str/interviewQuestions.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <set>
+
+using namespace std;
+
+// bool isUnique(string s) {
+//     set <char> myString;
+//     for (auto itr = s.begin(); itr != s.end(); itr++) {
+//         auto var = myString.insert((*itr));
+//         if (!var.second) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+int main(int argc, char ** argv) {
+    // Question 1:
+
+    // Ex: solosasy
+
+    // cout << isUnique(argv[1]) << std::endl;
+}
